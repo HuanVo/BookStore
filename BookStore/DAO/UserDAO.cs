181e851b17d243cd53119e01a0b397a1b7f80a3a
Vo Van Huan <huanoaz@gmail.com>
2017-12-15 11:57:14 +0700

    change things

---
+++ BookStore/DAO/UserDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace DAO
+{
+    public class UserDAO
+    {
+        BookStoreDbContext db = null;
+        private static UserDAO instance;
+        public static UserDAO Instance {
+            get
+            {
+                if (instance == null) instance = new UserDAO();
+                return UserDAO.instance;
+            }
+            private set { UserDAO.instance = value; }
+        }
+
+        private UserDAO()
+        {
+            db = new BookStoreDbContext();
+        }
+        
+
+    }
+}
